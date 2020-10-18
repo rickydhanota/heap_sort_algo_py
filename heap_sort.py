708cc592ed250ccc8fc730c941c752243804a1b6
Ricky <rickdhanota@gmail.com>
2020-10-18 13:55:23 -0700

    initial commit

---
+++ heap_sort.py
@@ -0,0 +1,9 @@
+# Heap Sort
+# Write a function that takes in an array of integers and returns a sorted array. Use the heap sort algorithm method to sort the array.
+# array = [8, 5, 2, 9, 5, 6, 3]
+# Output = [2, 3, 5, 5, 6, 8, 9]
+
+def heapSort(array):
+    pass
+
+print(heapSort([8, 5, 2, 9, 5, 6, 3]))
